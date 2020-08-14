e8209c3b2353bc00f3b5b0757fdbd959193d08ad
lytranuit <lytranuit@gmail.com>
2020-08-14 09:26:35 +0700

    config mail

--- application/views/email/baocao.blade.php
+++ application/views/email/baocao.blade.php
@@ -245,16 +245,16 @@
         </style>
     </head>
     <body>
-        <p><strong>1.Nhà cung cấp: </strong></p><div class='fr-view' style="white-space: pre"><?= $ncc ?></div>
-        <p><strong>2.Nhân sự tham gia: </strong></p><div class='fr-view' style="white-space: pre"><?= $nhansu ?></div>
-        <p><strong>3.Nhân sự khác: </strong></p><div class='fr-view' style="white-space: pre"><?= $nhansukhac ?></div>
-        <p><strong>4.Khách hàng chính: </strong></p><div><?= $listcustomer ?></div><div class='fr-view' style="white-space: pre"><?= $new_customer ?></div>
-        <p><strong>5.Sản phẩm chính: </strong></p><div><?= $listproduct ?></div><div class='fr-view' style="white-space: pre"><?= $new_product ?></div>
-        <p><strong>6.Thời gian bắt đầu: </strong><?= $date ?></p>
-        <p><strong>7.Thời gian kết thúc: </strong><?= $date_end ?></p>
-        <p><strong>8.Chia sẻ thông tin: </strong><?= $email_send ?></p>
-        <p><strong>9.Nội dung: </strong></p><div class='fr-view' style="white-space: pre"><?= $content ?></div>
-        <p><strong>10.Lưu ý đặc biệt: </strong></p><div class='fr-view' style="white-space: pre"><?= $note ?></div>
-        <p><strong>11.Từ khóa: </strong></p><div class='fr-view' style="white-space: pre"><?=$search ?></div>
+        <div><strong>1.Nhà cung cấp: </strong></div><div class='fr-view' style="white-space: pre"><?= $ncc ?></div>
+        <div><strong>2.Nhân sự tham gia: </strong></div><div class='fr-view' style="white-space: pre"><?= $nhansu ?></div>
+        <div><strong>3.Nhân sự khác: </strong></div><div class='fr-view' style="white-space: pre"><?= $nhansukhac ?></div>
+        <div><strong>4.Khách hàng chính: </strong></div><div><?= $listcustomer ?></div><div class='fr-view' style="white-space: pre"><?= $new_customer ?></div>
+        <div><strong>5.Sản phẩm chính: </strong></div><div><?= $listproduct ?></div><div class='fr-view' style="white-space: pre"><?= $new_product ?></div>
+        <div><strong>6.Thời gian bắt đầu: </strong><?= $date ?></div>
+        <div><strong>7.Thời gian kết thúc: </strong><?= $date_end ?></div>
+        <div><strong>8.Chia sẻ thông tin: </strong><?= $email_send ?></div>
+        <div><strong>9.Nội dung: </strong></div><div class='fr-view' style="white-space: pre"><?= $content ?></div>
+        <div><strong>10.Lưu ý đặc biệt: </strong></div><div class='fr-view' style="white-space: pre"><?= $note ?></div>
+        <div><strong>11.Từ khóa: </strong></div><div class='fr-view' style="white-space: pre"><?=$search ?></div>
     </body>
 </html>
